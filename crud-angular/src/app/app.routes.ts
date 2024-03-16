c52fe27d0b24b15db54c24f89c712adb59073452
= <=>
2024-03-15 21:35:00 -0300

    Add routing and lazy loading

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', pathMatch: 'full', redirectTo: 'courses'},
+{path: 'courses',
+loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)}];
