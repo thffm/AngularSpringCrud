0b45be385b3fce677eb77c9ca22f632e514583aa
= <=>
2024-03-21 21:05:59 -0300

    add table and card

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -9,6 +11,8 @@ import { CoursesRoutingModule } from './courses-routing.module';
   imports: [
     CommonModule,
     CoursesRoutingModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 
 })
